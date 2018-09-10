439f58d751b10dd4c50a1c833cbe0a3eee5e7fd0
李颖龙 <695498118@qq.com>
2018-09-10 23:49:52 +0800

    io

--- server/controls/user.js
+++ server/controls/user.js
@@ -9,29 +9,28 @@ export const register = async (user) => {
 	const newUser = new User({'username':name,'password':password})
 	try {
  		const query = await newUser.save()
- 		return {'state':SUCCESS,'msg':'注册成功'}
+ 		return {state:SUCCESS,msg:'注册成功'}
 	} 
 	catch(err) {
-		 return {'state':ERR,'msg':'注册失败'}
+		 return {state:ERR,msg:'注册失败'}
 	}
 }
 
 
 export const login = async (user) => {
 	const {name,password}  = user
-	console.log('ok')
 	const _user = await User.findOne({'username':name})
 
 	if(!_user) {
-		return {'state':1,'msg':'无此用户'}	
+		return {state:ERR,msg:'无此用户'}	
 	}
 	const data = _user.compare(password,_user.password)
 
 	if(data) {
 		const token = await signToken(user)
-		return {state:0,token,name,}
+		return {state:SUCCESS,token,name,}
 	}else {
-		return {'state':1,'msg':'登录失败'}
+		return {state:ERR,msg:'登录失败'}
 	}
 }
 
--- server/middiewares/socket.js
+++ server/middiewares/socket.js
@@ -1,19 +1,33 @@
 import socket from 'socket.io'
-
+const UserMap = new Map();
 export default (server)=> {
 		const io = socket(server)
-		let arr = ['lyl','123','1','3','5']
-		let num = 0
+
 		io.on('connection', async (socket)=> {
-			socket.id = arr[num]
-			num++
-			console.log(socket.id)
+
 			socket.on('news', (data,fn)=> {
 				console.log(data)
 				fn('woot');
-				socket.emit('server','surprise');
 			});
-			
-		});	
-		// io.sockets.socket('123').emit('server', ''); 
+			socket.on('login', (data)=> {
+					UserMap.set(data, socket.id);
+					console.log(UserMap)
+					console.log(UserMap.size); 
+			});
+			socket.on('server', (data)=> {
+					console.log(data)
+			});
+			socket.on('sendMsg', (data)=> {
+					console.log(data)
+					const id = UserMap.get(data.to)
+					io.to(id).emit('ggg','data.msg')
+			});
+
+
+
+			socket.on('disconnect', (data)=> {
+					console.log(data)
+			});
+
+		});	 
 }
